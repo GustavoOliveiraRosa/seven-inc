046415d09daf2892456be07acf0e9dc32c20ff94
Gustavo <gustavoolrosa2019@gmail.com>
2020-09-08 15:37:15 -0300

    Update Readme

--- Requeriments/configmysql.sql
+++ Requeriments/configmysql.sql
@@ -12,11 +12,37 @@ CREATE TABLE usuarios (
     PRIMARY KEY (id_usuario)
 ) CHARSET = utf8;
 
+
+
+insert into usuarios (email,senha) values ("gustavoolrosa2019@gmail.com","123");
+
+
+
 CREATE TABLE empregados (
     id INT (11) NOT NULL AUTO_INCREMENT COMMENT 'Id do cliente',
     nome VARCHAR (255) NOT NULL COMMENT 'Nome do usuário',
-    bornDate DateTime DEFAULT CURRENT_TIMESTAMP COMMENT 'Data de inicio',
+    bornDate DATE NOT NULL COMMENT 'Data de inicio',
     salary FLOAT (20) DEFAULT '0' COMMENT 'Salario do usuario',
     position VARCHAR (255) NOT NULL COMMENT 'Posição na empresa',
     PRIMARY KEY (id)
 ) CHARSET = utf8;
+
+insert into empregados 
+(nome, bornDate,salary,position) values
+("Gustavo1",STR_TO_DATE('30/04/2016','%d/%m/%Y'),1600.00,"Desenvolvedor Front Jr.");
+
+insert into empregados 
+(nome, bornDate,salary,position) values
+("Gustavo2",STR_TO_DATE('30/04/2016','%d/%m/%Y'),1600.00,"Desenvolvedor Front Jr.");
+
+insert into empregados 
+(nome, bornDate,salary,position) values
+("Gustavo3",STR_TO_DATE('30/04/2016','%d/%m/%Y'),1600.00,"Desenvolvedor Front Jr.");
+
+insert into empregados 
+(nome, bornDate,salary,position) values
+("Gustavo4",STR_TO_DATE('30/04/2016','%d/%m/%Y'),1600.00,"Desenvolvedor Front Jr.");
+
+insert into empregados 
+(nome, bornDate,salary,position) values
+("Gustavo5",STR_TO_DATE('30/04/2016','%d/%m/%Y'),1600.00,"Desenvolvedor Front Jr.");
