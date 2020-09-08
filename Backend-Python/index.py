c162a777a1d1a5eb2f30db4d49671c73b06b3278
Gustavo <gustavoolrosa2019@gmail.com>
2020-09-08 14:22:32 -0300

    API - Remodelagem DB + EP /employees

--- Backend-Python/index.py
+++ Backend-Python/index.py
@@ -14,7 +14,7 @@ from functions.mysql_connector import *
 app = Flask(__name__)
 
 # Validação de login de usuário
-@app.route('/Employees/login', methods=['POST'])
+@app.route('/employees/login', methods=['POST'])
 def login_Employees():
     data = request.get_json()
     content = request.json
@@ -25,6 +25,22 @@ def login_Employees():
             return jsonify(Status='Success', id=row[0]), 200
     else:
         return jsonify(Status='Fail'), 200
+
+
+# Lista todos os empregados
+@app.route('/employees', methods=['GET'])
+def listarEmpregados_Employees():
+    # Listando usuários por ordem de adição
+    comando = cmdmysql(str('select id,nome,DATE_FORMAT(bornDate, "%d/%m/%Y"),salary,position from empregados ORDER BY id desc'))
+    payload = []
+    content = {}
+    for result in comando:
+       content = {'id':result[0],'nome': result[1], 'bornDate': result[2],'salary':result[3] , 'position':result[4]}
+       payload.append(content)
+       content = {}
+    return jsonify(payload), 201
+
+
 
 
 
